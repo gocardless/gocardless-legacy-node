2dd4055c1f98b9055154b4f5241606a05f257256
Andrew Appleton <andrew@floatleft.com>
2013-11-06 14:09:17 +0000

    Ignore node_modules, add some things and some stuff

--- test/gocardless-test.js
+++ test/gocardless-test.js
@@ -1,13 +1,62 @@
+var sinon = require('sinon');
 var expect = require('expect.js');
+var mockery = require('mockery');
+
 var gocardless = require('../lib/gocardless.js');
+var Client = require('../lib/client');
 
 describe('gocardless', function() {
-  it('is an object', function() {
+
+  beforeEach(function() {
+    mockery.enable({
+      warnOnUnregistered: false,
+      useCleanCache: true
+    });
+  });
+
+  afterEach(function() {
+    mockery.resetCache();
+    mockery.disable();
+  });
+
+  it('is a function', function() {
     expect(gocardless).to.be.a(Function);
   });
 
-  it('throws an error if incorrect config options are passed', function() {
-    var opts = {};
-    expect(gocardless(opts)).to.throwError();
+  var opts;
+  describe('with incorrect config options', function() {
+    beforeEach(function() {
+      opts = {};
+    });
+
+    it('throws an error', function() {
+      expect(gocardless).withArgs(opts).to.throwError();
+    });
   });
+
+  describe('with correct config options', function() {
+    var ClientMock;
+    var gocardless;
+
+    beforeEach(function() {
+      ClientMock = sinon.spy(Client);
+      mockery.registerMock('./client', ClientMock);
+      gocardless = require('../lib/gocardless');
+
+      opts = { app_id: '', app_secret: '', token: '', merchant_id: '' };
+    });
+
+    afterEach(function() {
+      ClientMock.reset();
+    });
+
+    it('returns a new Client', function() {
+      var returned = gocardless(opts);
+      ClientMock.withArgs(opts);
+
+      expect(ClientMock.withArgs(opts).calledOnce).to.be.ok();
+      expect(returned).to.be.a(ClientMock)
+    });
+  });
+
 });
