8934456bfd6c03a03460172aae06180a1efd6994
Andrew Appleton <andrew@floatleft.com>
2013-11-09 18:13:15 +0000

    Resource passes callbacks to client

--- test/resources/resource-test.js
+++ test/resources/resource-test.js
@@ -18,14 +18,35 @@ describe('Resource', function() {
   });
 
   function itDelegatesToClient(fnName, httpMethod) {
-    it('calls client#request with method argument', function() {
-      resource[fnName](1);
-      expect(client.request.args[0][0].method).to.be(httpMethod);
+    describe('with an id', function() {
+      it('calls client#request with method argument', function() {
+        resource[fnName](1);
+        expect(client.request.args[0][0].method).to.be(httpMethod);
+      });
+
+      it('calls client#request with path and id', function() {
+        resource[fnName](1);
+        expect(client.request.args[0][0].path).to.be(basePath + '/' + 1);
+      });
+
+      it('passes the callback to the request', function() {
+        function cb() {};
+        resource[fnName](1, cb);
+        expect(client.request.args[0][1]).to.be(cb);
+      });
     });
 
-    it('calls client#request with path and id', function() {
-      resource[fnName](1);
-      expect(client.request.args[0][0].path).to.be(basePath + '/' + 1);
+    describe('with no id', function() {
+      it('requests the index', function() {
+        resource[fnName]();
+        expect(client.request.args[0][0].path).to.be(basePath);
+      });
+
+      it('passes the callback to the request', function() {
+        function cb() {};
+        resource[fnName](cb);
+        expect(client.request.args[0][1]).to.be(cb);
+      });
     });
   }
 
@@ -41,6 +62,14 @@ describe('Resource', function() {
       resource.put(1, params);
       expect(client.request.args[0][0].json).to.be(params);
     });
+
+    describe('with params and an id', function() {
+      it('passes the callback to the request', function() {
+        function cb() {};
+        resource.put(1, { some: 'params' }, cb);
+        expect(client.request.args[0][1]).to.be(cb);
+      });
+    });
   });
 
   describe('#post', function() {
@@ -50,6 +79,14 @@ describe('Resource', function() {
       var params = { some: 'params' };
       resource.put(1, params);
       expect(client.request.args[0][0].json).to.be(params);
+    });
+
+    describe('with params and an id', function() {
+      it('passes the callback to the request', function() {
+        function cb() {};
+        resource.post(1, { some: 'params' }, cb);
+        expect(client.request.args[0][1]).to.be(cb);
+      });
     });
   });
 
