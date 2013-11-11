101f66ee0c97a48f843c24b8340462570a7b03de
Andrew Appleton <andrew@floatleft.com>
2013-11-11 18:50:55 +0000

    Refactor Resource methods to have consistant signatures.

--- test/resources/resource-test.js
+++ test/resources/resource-test.js
@@ -1,6 +1,8 @@
 var sinon = require('sinon');
 var expect = require('expect.js');
 
+expect = require('sinon-expect').enhance(expect, sinon, 'was');
+
 var Resource = require('../../lib/resources/resource');
 
 describe('Resource', function() {
@@ -22,12 +24,12 @@ describe('Resource', function() {
   function itDelegatesToClient(fnName, httpMethod) {
     describe('with an id', function() {
       it('calls client#request with method argument', function() {
-        resource[fnName](1);
+        resource[fnName]({ id: 1 });
         expect(client.request.args[0][0].method).to.be(httpMethod);
       });
 
       it('calls client#request with path and id', function() {
-        resource[fnName](1);
+        resource[fnName]({ id: 1 });
         expect(client.request.args[0][0].path).to.be(basePath + '/' + 1);
       });
 
@@ -50,6 +52,12 @@ describe('Resource', function() {
         expect(client.request.args[0][1]).to.be(cb);
       });
     });
+
+    it('accepts a path option', function() {
+      var path = '/some/other/path';
+      resource[fnName]({ path: path });
+      expect(client.request).was.calledWith({ method: httpMethod, path: path });
+    });
   }
 
   describe('#get', function() {
@@ -61,34 +69,18 @@ describe('Resource', function() {
 
     it('passes JSON params', function() {
       var params = { some: 'params' };
-      resource.put(1, params);
+      resource.put({ id: 1, json: params });
       expect(client.request.args[0][0].json).to.be(params);
-    });
-
-    describe('with params and an id', function() {
-      it('passes the callback to the request', function() {
-        function cb() {};
-        resource.put(1, { some: 'params' }, cb);
-        expect(client.request.args[0][1]).to.be(cb);
-      });
     });
   });
 
   describe('#post', function() {
     itDelegatesToClient('post', 'POST');
 
-    it('passes stringified params', function() {
+    it('passes JSON params', function() {
       var params = { some: 'params' };
-      resource.put(1, params);
+      resource.post({ id: 1, json: params });
       expect(client.request.args[0][0].json).to.be(params);
-    });
-
-    describe('with params and an id', function() {
-      it('passes the callback to the request', function() {
-        function cb() {};
-        resource.post(1, { some: 'params' }, cb);
-        expect(client.request.args[0][1]).to.be(cb);
-      });
     });
   });
 
